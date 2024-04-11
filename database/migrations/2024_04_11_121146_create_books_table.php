ea4e6a22fa4e0feaa357fb9eb66c45d54f849ebf
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-11 14:52:15 +0200

    inserito colonne nella migrazione books

--- database/migrations/2024_04_11_121146_create_books_table.php
+++ database/migrations/2024_04_11_121146_create_books_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->text("description")->nullable();
+            $table->binary("thumb");
+            $table->decimal("price", 8, 2);
+            $table->string("series");
+            $table->date("sale_date");
+            $table->string("type");
+            $table->json("artists");
+            $table->json("writers");
             $table->timestamps();
         });
     }
