59dfe858b7ab298c75626035e6bacde524390109
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-11 16:04:02 +0200

    All'interno di bookseeder ho modificato le proprietà, ho poi effettuato le migrazioni del seeder.
    Ho creato poi un controller resource nominato BookController

--- database/migrations/2024_04_11_121146_create_books_table.php
+++ database/migrations/2024_04_11_121146_create_books_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
             $table->string("title");
             $table->text("description")->nullable();
             $table->binary("thumb");
-            $table->decimal("price", 8, 2);
+            $table->string("price");
             $table->string("series");
             $table->date("sale_date");
             $table->string("type");
-            $table->json("artists");
-            $table->json("writers");
+            $table->text("artists");
+            $table->text("writers");
             $table->timestamps();
         });
     }
--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Book;
 
 class BookSeeder extends Seeder
 {
@@ -12,6 +13,25 @@ class BookSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $books = config("books");
+
+        foreach ($books as $book) {
+
+            $newBook = new Book();
+
+            $newBook->title = $book["title"];
+            $newBook->description = $book["description"];
+            $newBook->thumb = $book["thumb"];
+            
+            
+            $newBook->price = $book["price"];
+            $newBook->series = $book["series"];
+            $newBook->sale_date = $book["sale_date"];
+            $newBook->type = $book["type"];
+            $newBook->artists = implode(' , ', $book['artists']);
+            $newBook->writers = implode(' , ', $book['writers']);
+
+            $newBook->save();
+        }
     }
 }
