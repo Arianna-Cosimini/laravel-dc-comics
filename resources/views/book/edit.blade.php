f63237b7d772fb7024d75019661edc74d03c1eae
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-12 13:03:24 +0200

    in edit blade ho inserito il form  utilizzando value per inserire il contenuto all'interno degli input del form

---
+++ resources/views/book/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container py-5">
+        <h1 class="mb-4">Modifica il contenuto del libro</h1>
+
+
+        <form action="{{ route('books.update', $book->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="title" name="title" value="{{ $book->title }}" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="description" class="form-label">Descrizione</label>
+                <textarea type="text" class="form-control" id="description" name="description" rows="4">{{ $book->description }}</textarea>
+            </div>
+
+            <div class="mb-3">
+                <label for="type" class="form-label">Tipologia</label>
+                <input type="text" class="form-control" id="type" name="type" value="{{ $book->type }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="src" class="form-label">Tipo dis serie</label>
+                <input type="text" class="form-control" id="src" name="src" value="{{ $book->src }}">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Salva</button>
+
+        </form>
+    </div>
+@endsection
