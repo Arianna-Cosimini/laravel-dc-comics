684d4b0aa8867c68faa632d83ce29390f8fda46c
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-12 12:57:47 +0200

    In views>book ho creato nuova pagina edit.blade.php

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -58,9 +58,10 @@ class BookController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Book $book)
     {
-        //
+        return view('book.edit');
+        
     }
 
     /**
