d47dbd9dd6d442ff36d860a8b48db59c1e0d2518
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-15 13:54:23 +0200

    in create.blade inserito sotto ogni campo del form i controllo di errore

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -12,22 +12,46 @@
 
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control" id="title" name="title" required>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                    value="{{ old('title') }}" required>
+                @error('title')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea type="text" class="form-control" id="description" name="description"></textarea>
+                <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description"
+                    name="description">{{ old('description') }}"</textarea>
+                @error('description')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="type" class="form-label">Tipologia</label>
-                <input type="text" class="form-control" id="type" name="type">
+                <input type="text" class="form-control @error('type') is-invalid @enderror" id="type"
+                    value="{{ old('type') }}" name="type">
+                @error('type')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="cooking-time" class="form-label">Tipo di serie</label>
-                <input type="text" class="form-control" id="cooking-time" name="cooking-time">
+                <input type="text" class="form-control @error('series') is-invalid @enderror" id="series"
+                    name="series" value="{{ old('series') }}">
+                @error('series')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <button type="submit" class="btn btn-primary">Salva</button>
